96512b65d7c142cc8de656200fe29caac6a72000
wujinli <wujinli>
2019-12-31 15:59:54 +0800

    添加bean的作用域，bean的注解

---
+++ spring_autowired/src/main/java/Cat.java
@@ -0,0 +1,5 @@
+public class Cat {
+    public void  sound(){
+        System.out.println("miaomiao....");
+    }
+}
--- spring_di/src/test/java/TestMain.java
+++ spring_di/src/test/java/TestMain.java
@@ -1,13 +1,49 @@
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 //测试依赖注入
 public class TestMain {
-
-    public static void main(String[] args) {
+    @Test
+    public void student() {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println(student);
+    }
 
-        Student student= (Student) context.getBean("student");
+    //拓展命名空间p
+    @Test
+    public void user_p() {
+        //p命名扩展空间
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbean.xml");
+        User user = context.getBean("user", User.class);
+        System.out.println(user);
+    }
 
-        System.out.println(student.toString());
+    //拓展命名空间c
+    @Test
+    public void user_c() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbean.xml");
+        User user = context.getBean("user_c", User.class);
+        System.out.println(user);
+    }
+
+
+    //bean的作用域 默认单例模式
+    @Test
+    public void user_scopes_single() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbean.xml");
+        User user = context.getBean("user_c", User.class);
+        User user2 = context.getBean("user_c", User.class);
+        System.out.println(user == user2);
+    }
+
+    //bean的作用域 prototype
+    @Test
+    public void user_scopes_prototype() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbean.xml");
+        User user = context.getBean("user_p", User.class);
+        User user2 = context.getBean("user_p", User.class);
+        System.out.println(user == user2);
     }
 }
