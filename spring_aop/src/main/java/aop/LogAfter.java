327ca97a980d14d8a6e80c8ce98375ee3319f4a1
wujinli <269316671@qq.COM>
2020-01-12 22:03:41 +0800

    添加aop实现的xml配置方式

--- spring_aop/src/main/java/aop/LogAfter.java
+++ spring_aop/src/main/java/aop/LogAfter.java
@@ -4,6 +4,16 @@ import org.springframework.aop.AfterReturningAdvice;
 
 import java.lang.reflect.Method;
 
+/**
+ * aop面向切面编程
+ * 1.xml进行切面配置
+ * a.xml配置文件进行aop的导包；
+ * b.aop实现原理是动态代理，即代理的是接口
+ * b-1.创建接口，接口实现类，业务实现类
+ * b-2.创建aop实现的方法添加比如执行原方法之前或者之后进行一些业务操作，在不该原有代码马基础上进行新业务的添加
+ * c.在xml中进行这些类的的自动注册，配置到ioc容器中
+ * d.在xml配置文件中进行aop切面的注册
+ */
 public class LogAfter implements AfterReturningAdvice {
     /**
      * @param returnValue 方法返回值
